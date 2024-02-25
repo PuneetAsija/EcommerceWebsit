888915ea25aa3a08adadf7a8bb106d527180de8a
PuneetAsija <144670551+PuneetAsija@users.noreply.github.com>
2024-02-25 22:02:32 +0530

    Add files via upload

---
+++ i.js
@@ -0,0 +1,29 @@
+// let n = 5;
+// let ans = 1;
+// for(let i=n; i >= 1; i--){
+//     // ans = ans*i;
+//     ans *= i;
+// }
+// console.log(ans)
+
+// let n= 5;
+// for(let i=1; i <=10; i++){
+//     console.log(n*i)
+// }
+
+// let n = 6;
+// let ans = 1;
+
+// function fact(n,ans){
+//     if(n===1){
+//         console.log(ans);
+//         return;
+//     }
+//     ans *= n;
+//     n--;
+//     fact(n,ans);
+// }
+
+// fact(n, ans);
+// console.log(h);
+
